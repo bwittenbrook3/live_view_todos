31a87fe783bea0725b3d3fc0af3e33588b4e60f3
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2019-10-29 01:19:47 -0400

    first commit

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,30 @@
+defmodule LiveViewTodosWeb.TodoLive do
+  use Phoenix.LiveView
+
+  alias LiveViewTodos.Todos
+  alias LiveViewTodosWeb.TodoView
+
+  def mount(_session, socket) do
+    Todos.subscribe()
+    {:ok, fetch(socket)}
+  end
+
+  def handle_event("add", %{"todo" => todo}, socket) do
+    Todos.create_todo(todo)
+
+    {:noreply, fetch(socket)}
+  end
+
+  def handle_info({Todos, [:todo | _], _}, socket) do
+    {:noreply, fetch(socket)}
+  end
+
+  def render(assigns) do
+    TodoView.render("todos.html", assigns)
+  end
+
+  defp fetch(socket) do
+    assign(socket, todos: Todos.list_todos())
+  end
+
+end
